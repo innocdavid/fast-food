2b1440c85fe3a2c242714bff0206b42eb3cc0e0e
Banja David Lotigo <innocdavid@gmail.com>
2024-05-24 22:43:22 +0300

    [client] add global.css and remove app.css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import AppRoutes from './AppRoutes'
 
-import './index.css'
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
