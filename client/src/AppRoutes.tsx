8e6d95ff9bb0faf5a2edf4f206a3dc15d1ab259b
Banja David Lotigo <innocdavid@gmail.com>
2024-05-24 22:42:42 +0300

    [client] fix home route

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Layout from '@/layouts/Layout';
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route  path='/' element={ <span>HOME PAGE</span> } />
+            <Route  path='/' element={ <Layout>HOME PAGE</Layout> } />
             <Route  path='/user-profile' element={ <span>USER PROIFLE PAGE</span> } />
-            <Route  path='*' element={ <Navigate to='/'/> } />
+            <Route  path='*' element={ <Navigate to='/' /> } />
         </Routes>
     );
 }
