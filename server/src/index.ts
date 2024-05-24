f6e3f1c2eb282071c1fba43df227accf170898bc
Banja David Lotigo <innocdavid@gmail.com>
2024-05-24 21:59:06 +0300

    [server] add a commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,9 @@ import 'dotenv/config';
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     .then(() => {
         console.log('conected to database');
-    })
+    });
 
+//app
 const app = express();
 app.use(express.json());
 app.use(cors());
