a998922ae7238b23bf39f04885dbe07b4edf34df
Banja David Lotigo <innocdavid@gmail.com>
2024-05-25 22:29:33 +0300

    [client] add username menu to handle login logout

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -1,18 +1,26 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button"
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
 
     const handleLoginWithRedirect = async () => await loginWithRedirect();
     return (
-        <Button 
-            variant="ghost" 
-            className="font-bold hover:text-orange-500 hover:bg-white"
-            onClick={handleLoginWithRedirect}
-        >
-            Login
-        </Button>
+        <span className="flex space-x-2 items-center">
+            {isAuthenticated ? (
+                <UsernameMenu />
+            ) : (
+                <Button 
+                    variant="ghost" 
+                    className="font-bold hover:text-orange-500 hover:bg-white"
+                    onClick={handleLoginWithRedirect}
+                >
+                    Login
+                </Button>
+            )}
+        </span>
+        
     )
 }
 
