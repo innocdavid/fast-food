787b578a51e644ad4741da0656903752b955dec1
Banja David Lotigo <innocdavid@gmail.com>
2024-05-26 21:12:46 +0300

    [server] add user route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import mongoose, { mongo } from 'mongoose';
+import myUserRoute from './routes/MyUserRoute'
 import cors from 'cors';
 import 'dotenv/config';
 
@@ -14,9 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', async (req: Request, res: Response) => {
-    res.json({ message: 'Hello from the server' });
-});
+app.use('/api/my/user', myUserRoute)
 
 app.listen(8000, () => {
     console.log('server running on localhost:8000');
