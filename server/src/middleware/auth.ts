a132ff27f00974143dd697e2c0ab62f3d08b8ac8
Banja David Lotigo <innocdavid@gmail.com>
2024-05-28 21:48:24 +0300

    [client] format code

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { auth } from "express-oauth2-jwt-bearer";
 import 'dotenv/config';
 
 export const jwtCheck = auth({
-    audience: process.env.AUTH0_AUDIENCE,
-    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-    tokenSigningAlg: 'RS256'
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  tokenSigningAlg: 'RS256'
 });
