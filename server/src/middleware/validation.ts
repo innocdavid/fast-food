d481f68a5d1f6914ecce081fe72eb0608eece73d
Banja David Lotigo <innocdavid@gmail.com>
2024-06-14 13:50:33 +0300

    [server] add a restaurant model field validation

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -34,3 +34,37 @@ export const validateMyUserRequest = [
         .withMessage('Country must be a string'),
     handleValidationErrors,
 ];
+
+export const validateRestaurantRequest = [
+    body('restaurantName')
+        .notEmpty()
+        .withMessage('Restaurant name is required'),
+    body('city')
+        .notEmpty()
+        .withMessage('Citry is required'),
+    body('country')
+        .notEmpty()
+        .withMessage('Country is required'),
+    body('deliveryPrice')
+        .isFloat({ min: 0 })
+        .withMessage('Delivery price must be a positive number'),
+    body('estimatedDeliveryTime')
+        .isInt({ min: 0 })
+        .withMessage('Estimated delivery time must be a postive number'),
+    body('cuisines')
+        .isArray()
+        .withMessage('Cuisines nust be a list')
+        .not()
+        .isEmpty()
+        .withMessage('Cusine list can not be empty'),
+    body('menuItem')
+        .isArray()
+        .withMessage('Menu items must be a list'),
+    body('menuItems.*.name')
+        .notEmpty()
+        .withMessage('Menu item name is required'),
+    body('menuItems.*.price')
+        .isFloat({ min: 0 })
+        .withMessage('Menu item price must be a number and not empty'),
+        handleValidationErrors
+];
