1a91d6165e0faa557365e4247ae28cd87495261d
Banja David Lotigo <innocdavid@gmail.com>
2024-06-14 13:51:05 +0300

    [server] add middleware to create my restaurant route

--- server/src/routes/MyRestaurantRoute.ts
+++ server/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import multer from 'multer';
 import MyRestaurantController from '../controllers/MyRestaurantController';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateRestaurantRequest } from '../middleware/validation';
 
 const router = express.Router();
 
@@ -15,6 +17,9 @@ const upload = multer({
 router.post(
     '/', 
     upload.single('imageFile'),
+    validateRestaurantRequest,
+    jwtCheck,
+    jwtParse,
     MyRestaurantController.createMyRestaurant
 );
 
