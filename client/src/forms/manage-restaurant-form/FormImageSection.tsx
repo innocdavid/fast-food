a0fcaa6a196ad95a26ff3c3043310751777d15db
Banja David Lotigo <innocdavid@gmail.com>
2024-06-18 15:33:24 +0300

    [client] add image aspect ration component

--- client/src/forms/manage-restaurant-form/FormImageSection.tsx
+++ client/src/forms/manage-restaurant-form/FormImageSection.tsx
@@ -26,14 +26,14 @@ const FormImageSection = () => {
       </div>
 
       <div className="flex flex-col gap-8 md:w-[50%]">
-        {/* {existingImageUrl && (
+        {existingImageUrl && (
           <AspectRatio ratio={16 / 9}>
             <img
               src={existingImageUrl}
               className="rounded-md object-cover h-full w-full"
             />
           </AspectRatio>
-        )} */}
+        )}
         <FormField
           control={control}
           name="imageFile"
