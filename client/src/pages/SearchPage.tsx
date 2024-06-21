02a77f14f7a9285256a825e0f1abd8ce786109f1
Banja David Lotigo <innocdavid@gmail.com>
2024-06-21 12:22:07 +0300

    [client] update the search page

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,11 +1,26 @@
+import { useSearchRestaurant } from "@/api/RestaurantApi";
+import SearchResultCard from "@/components/SearchResultsCard";
+import SearchResultsInfo from "@/components/SearchResultsInfo";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
     const { city } = useParams();
+    const { results, isLoading } = useSearchRestaurant(city);
+
+    if (isLoading) return <span>Loading....</span>
+
+    if (!results?.data || !city) return <span>No results found</span>
+    console.log(results)
     return (
-        <>
-            <h1>User searched for {city}</h1>
-        </>
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div id="cuisines-list">insert cuisines here :)</div>
+            <div id="main-content" className="flex flex-col gap-5">
+                <SearchResultsInfo totalCount={results.pagination.totalCount} city={city} />
+                {results.data.map((restaurant) => (
+                    <SearchResultCard restaurant={restaurant}/>
+                ))}
+            </div>
+        </div>
     );
 };
 
