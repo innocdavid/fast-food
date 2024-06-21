7d8840f78abd678a8e4d5ca7430e185581832971
Banja David Lotigo <innocdavid@gmail.com>
2024-06-21 12:07:58 +0300

    [client] add a search restaurant hook

---
+++ client/src/api/RestaurantApi.tsx
@@ -0,0 +1,21 @@
+import { SearchRestaurantResponse } from "@/types";
+import { useQuery } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useSearchRestaurant = (city?: string) => {
+    const createSearchRequest = async (): Promise<SearchRestaurantResponse> => {
+        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}`);
+
+        if (!response.ok) throw new Error("Faild to get restaurant");
+
+        return response.json();
+    };
+
+    const { data: results, isLoading} = useQuery(
+        ["searchRestaurant"], 
+        createSearchRequest,
+        { enabled: !!city });
+
+    return { results, isLoading };
+}
