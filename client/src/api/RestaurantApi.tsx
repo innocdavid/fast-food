f74de27d6204fc5bcd4d7efbe6a514d361a3cce2
Banja David Lotigo <innocdavid@gmail.com>
2024-06-24 15:41:31 +0300

    [client] search search for cusine and restaurant name query

--- client/src/api/RestaurantApi.tsx
+++ client/src/api/RestaurantApi.tsx
@@ -1,11 +1,16 @@
+import { SearchState } from "@/pages/SearchPage";
 import { SearchRestaurantResponse } from "@/types";
 import { useQuery } from "react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurant = (city?: string) => {
+export const useSearchRestaurant = (searchState: SearchState, city?: string) => {
     const createSearchRequest = async (): Promise<SearchRestaurantResponse> => {
-        const response = await fetch(`${API_BASE_URL}/api/restaurant/search/${city}`);
+        const params = new URLSearchParams();
+        params.set("searchQuery", searchState.searchQuery);
+
+        const response = await fetch(
+            `${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`);
 
         if (!response.ok) throw new Error("Faild to get restaurant");
 
@@ -13,7 +18,7 @@ export const useSearchRestaurant = (city?: string) => {
     };
 
     const { data: results, isLoading} = useQuery(
-        ["searchRestaurant"], 
+        ["searchRestaurant", searchState], 
         createSearchRequest,
         { enabled: !!city });
 
