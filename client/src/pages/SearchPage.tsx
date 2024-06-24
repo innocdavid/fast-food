98a28824b4cc5fb3f1eb7b1f0e3f4b466817c76d
Banja David Lotigo <innocdavid@gmail.com>
2024-06-24 15:42:26 +0300

    [client] add search component on search page

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,20 +1,50 @@
 import { useSearchRestaurant } from "@/api/RestaurantApi";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultsCard";
 import SearchResultsInfo from "@/components/SearchResultsInfo";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+
+export type SearchState = {
+    searchQuery: string;
+};
 
 const SearchPage = () => {
     const { city } = useParams();
-    const { results, isLoading } = useSearchRestaurant(city);
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: "",
+    });
+    const { results, isLoading } = useSearchRestaurant(searchState, city);
+
+    const setSearchQuery = (searchFormData: SearchForm) => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: searchFormData.searchQuery,
+        }));
+    
+    };
+
+    const resetSearch = () => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            setSearchQuery: "",
+        }));
+    };
 
     if (isLoading) return <span>Loading....</span>
 
     if (!results?.data || !city) return <span>No results found</span>
-    console.log(results)
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div id="cuisines-list">insert cuisines here :)</div>
             <div id="main-content" className="flex flex-col gap-5">
+                <SearchBar 
+                    searchQuery={searchState.searchQuery}
+                    onSubmit={setSearchQuery} 
+                    placeHolder="Search by cuisine or restauant name"
+                    onReset={resetSearch}
+                />
                 <SearchResultsInfo totalCount={results.pagination.totalCount} city={city} />
                 {results.data.map((restaurant) => (
                     <SearchResultCard restaurant={restaurant}/>
