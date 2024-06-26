94802c396f083790d23aa04cd9b774e61b164b70
Banja David Lotigo <innocdavid@gmail.com>
2024-06-26 12:41:21 +0300

    [client] update componenet and pages to include adding and removing items to card

--- client/src/components/RestaurantDetailsMenuItem.tsx
+++ client/src/components/RestaurantDetailsMenuItem.tsx
@@ -3,11 +3,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 type Props = {
     menuItem: MenuItem;
+    addToCart: () => void;
 };
 
-const RestaurantDetailsMenuItem = ({ menuItem }: Props) => {
+const RestaurantDetailsMenuItem = ({ menuItem, addToCart }: Props) => {
     return (
-        <Card className="cursor-pointer">
+        <Card className="cursor-pointer" onClick={addToCart}>
             <CardHeader className="font-bold">
                 <CardTitle>{menuItem.name}</CardTitle>
             </CardHeader>
