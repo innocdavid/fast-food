8f1d32b884047a4d699b3c17f02eedadf06b1dc6
Banja David Lotigo <innocdavid@gmail.com>
2024-06-26 09:23:23 +0300

    [client] update route to details page

--- client/src/components/SearchResultsCard.tsx
+++ client/src/components/SearchResultsCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 const SearchResultCard = ({ restaurant }: Props) => {
   return (
     <Link
-      to={`/detail/${restaurant._id}`}
+      to={`/details/${restaurant._id}`}
       className="grid lg:grid-cols-[2fr_3fr] gap-5 group"
     >
       <AspectRatio ratio={16 / 6}>
