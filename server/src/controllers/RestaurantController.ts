19f216a6a3b77adb0502f29f1350cf5653fefdf6
Banja David Lotigo <innocdavid@gmail.com>
2024-06-26 08:44:51 +0300

    [server] add a controller to get a restaurant by their id

--- server/src/controllers/RestaurantController.ts
+++ server/src/controllers/RestaurantController.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from "express";
 import Restaurant from "../models/restaurant";
 
+const getRestaurant = async (req: Request, res: Response) => {
+    try {
+        const { restaurantId } = req.params;
+
+        const restaurant = await Restaurant.findById(restaurantId);
+
+        if (!restaurant) return res.status(404).json({ message: "Restaurant not found" });
+
+        res.status(200).json(restaurant);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
 const searchRestaurant = async (req: Request, res: Response) => {
     try {
         const city = req.params.city;
@@ -72,5 +88,6 @@ const searchRestaurant = async (req: Request, res: Response) => {
 };
 
 export default {
+    getRestaurant,
     searchRestaurant,
 }
