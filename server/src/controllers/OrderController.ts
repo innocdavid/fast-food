be90a9fdb46342ab01d2b3ce81abf7711047911b
Banja David Lotigo <innocdavid@gmail.com>
2024-06-28 12:13:26 +0300

    [server] save order after stripe checkout

--- server/src/controllers/OrderController.ts
+++ server/src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { Request, Response } from "express";
 import Restaurant, { MenuItemType } from "../models/restaurant";
+import Order from "../models/order";
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
@@ -31,16 +32,28 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
         if (!restaurant) throw new Error("Restaurant not found");
 
+        const newOrder = new Order({
+            restaurant: restaurant,
+            user: req.userId,
+            status: "placed",
+            deliveryDetails: checkoutSessionRequest.deliveryDetails,
+            cartItems: checkoutSessionRequest.cartItems,
+            createAt: new Date(),
+        });
+
         const lineItems = createLineItems(checkoutSessionRequest, restaurant.menuItems);
 
         const session = await createSession(
             lineItems, 
-            "TEST_ORDER_ID",
+            newOrder._id.toString(),
             restaurant.deliveryPrice,
             restaurant._id.toString()
         );
 
         if (!session.url) return res.status(500).json({ message: "Error creating stripe session" });
+
+
+        await newOrder.save();
 
         res.json({ url: session.url });
 
