5d5d9e028fa916cb2f90c3f35a4559e340bd74f6
Banja David Lotigo <innocdavid@gmail.com>
2024-07-01 09:55:31 +0300

    [server] add a controller to fetch user orders

--- server/src/controllers/OrderController.ts
+++ server/src/controllers/OrderController.ts
@@ -23,6 +23,22 @@ type CheckoutSessionRequest = {
     restaurantId: string;
 };
 
+const getMyOrders = async (req: Request, res: Response) => {
+    try {
+        const orders = await Order.find({ user: req.userId })
+            .populate("restaurant")
+            .populate("user");
+
+        if (!orders) return res.status(404).json({ message: "Order not found" });
+
+        res.status(200).json(orders);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
 const stripeWebhookHandler = async (req: Request, res: Response) => {
     let event;
     try {
@@ -154,6 +170,7 @@ const createSession = async (
   };
 
 export default {
+    getMyOrders,
     stripeWebhookHandler,
     createCheckoutSession,
 }
