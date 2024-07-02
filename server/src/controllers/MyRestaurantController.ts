ef2988da11d6c95b481c176b726b0ff3afc1ea11
Banja David Lotigo <innocdavid@gmail.com>
2024-07-02 12:41:53 +0300

    [server] add a get my restaurant orders controller

--- server/src/controllers/MyRestaurantController.ts
+++ server/src/controllers/MyRestaurantController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import cloudinary from 'cloudinary';
 import Restaurant from '../models/restaurant';
 import mongoose from 'mongoose';
+import Order from '../models/order';
 
 
 const getMyRestaurant = async (req: Request, res: Response) => {
@@ -82,7 +83,24 @@ const updateMyRestaurant = async (req: Request, res: Response) => {
         console.error(error);
         res.status(500).json({ message: "Something went wrong" });
     }
-}
+};
+
+const getMyRestaurantOrders = async (req: Request, res: Response) => {
+    try {
+      const restaurant = await Restaurant.findOne({ user: req.userId });
+
+      if (!restaurant) return res.status(404).json({ message: "Restaurant not found" });
+  
+      const orders = await Order.find({ restaurant: restaurant._id })
+        .populate("restaurant")
+        .populate("user");
+  
+      res.json(orders);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  };
 
 const uploadImageFile = async (file: Express.Multer.File) => {
     const image = file;
@@ -92,9 +110,10 @@ const uploadImageFile = async (file: Express.Multer.File) => {
     const uploadResponse = await cloudinary.v2.uploader.upload(dataURI);
 
     return uploadResponse.url;
-}
+};
 
 export default {
+    getMyRestaurantOrders,
     getMyRestaurant,
     createMyRestaurant,
     updateMyRestaurant,
