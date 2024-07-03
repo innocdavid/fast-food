d0462388341f6ece081172e393c1aadbbd0bbc63
Banja David Lotigo <innocdavid@gmail.com>
2024-07-03 10:35:17 +0300

    [client] update restaurant order status

--- client/src/components/OrderItemCard.tsx
+++ client/src/components/OrderItemCard.tsx
@@ -1,16 +1,33 @@
-import { Order } from "@/types";
+import { Order, OrderStatus } from "@/types";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Separator } from "./ui/separator";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { ORDER_STATUS } from "@/config/order-status-config";
+import { useUpdateMyRestaurantOrder } from "@/api/MyRestaurantApi";
+import { useEffect, useState } from "react";
 
 type Props = {
     order: Order;
 }
 
 const OrderItemCard = ({ order }: Props) => {
+    const { updateRestaurantStatus, isLoading } = useUpdateMyRestaurantOrder();
+    const [status, setStatus] = useState<OrderStatus>(order.status);
+
+    useEffect(() => {
+        setStatus(order.status);
+    }, [order.status]);
+
+    const handleStatusChange = async (newStatus: OrderStatus) => {
+        await updateRestaurantStatus({
+          orderId: order._id as string,
+          status: newStatus,
+        });
+        setStatus(newStatus);
+    };
+    
     const getTime = () => {
         const orderDateTime = new Date(order.createdAt);
     
@@ -70,7 +87,11 @@ const OrderItemCard = ({ order }: Props) => {
                 </div>
                 <div className="flex flex-col space-y-1.5">
                     <Label htmlFor="status">What is the status fo this order?</Label>
-                    <Select>
+                    <Select
+                        value={status}
+                        disabled={isLoading}
+                        onValueChange={(value) => handleStatusChange(value as OrderStatus)}
+                    >
                         <SelectTrigger id="status">
                             <SelectValue placeholder="Status" />
                         </SelectTrigger>
